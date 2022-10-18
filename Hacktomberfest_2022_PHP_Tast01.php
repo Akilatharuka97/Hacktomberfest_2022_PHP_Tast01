9e3397d4c350c474e867df981c1c652d0024df5d
Ushan Perera <48118322+l-MONDY-l@users.noreply.github.com>
2022-10-18 16:00:41 +0530

    Update Hacktomberfest_2022_PHP_Tast01.php

--- Hacktomberfest_2022_PHP_Tast01.php
+++ Hacktomberfest_2022_PHP_Tast01.php
@@ -1,11 +1,10 @@
 <?php
 $t = date("H");
-
-if (t < "10") {
-  echo "Have a good morning!"
+if ($t < "10") {
+ echo "Have a good morning!";
 } elseif ($t < "20") {
-  echo "Have a good day!";
-} else 
-  echo "Have a good night!"
+ echo "Have a good day!";
+} else { //this is missing..
+ echo "Have a good night!";
 }
 ?>
