def4252c4d77130527b8e6a59022fe8a2833cb72
HarshaDeZoysa <116942647+HarshaDeZoysa@users.noreply.github.com>
2022-10-29 13:08:22 +0530

    Update Hacktomberfest_2022_PHP_Tast01.php

--- Hacktomberfest_2022_PHP_Tast01.php
+++ Hacktomberfest_2022_PHP_Tast01.php
@@ -1,11 +1,13 @@
 <?php
 $t = date("H");
-
-if (t < "10") {
-  echo "Have a good morning!"
+if ($t < "10") {
+ echo "Have a good morning!";
+  //display text
 } elseif ($t < "20") {
-  echo "Have a good day!";
-} else 
-  echo "Have a good night!"
+ echo "Have a good day!";
+ //display text 
+} else {
+ echo "Have a good night!";
+  //display text
 }
 ?>
